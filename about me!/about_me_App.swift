d3aedb925e2e7beb57726d08eb10098ca067f9c6
shrikap <shrikapunuganti@gmail.com>
2023-07-13 13:03:18 -0700

    Initial Commit

---
+++ about me!/about_me_App.swift
@@ -0,0 +1,17 @@
+//
+//  about_me_App.swift
+//  about me!
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct about_me_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
